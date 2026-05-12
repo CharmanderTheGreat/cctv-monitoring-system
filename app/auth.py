8e364914d83b8aeb0634dba22ae944c16c18fa9b
jeorgrenaissnc <jeorginanylag.resurreccion@gmail.com>
2026-05-12 12:08:16 +0800

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,5 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session
+from flask_login import login_user, logout_user, login_required, current_user
 from app.models import (
     User,
     LoginAttempt,
